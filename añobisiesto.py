960adcf19902ac4495450199beb0d55a0d3e5699
raulperalta48743-glitch <raulperalta48743@gmail.com>
2026-03-10 10:13:35 -0600

    Modify print statements for leap year logic
    Updated output messages for leap year check.

--- añobisiesto.py
+++ añobisiesto.py
@@ -5,6 +5,7 @@ a = int(input("\nIngresa un año: "))
 if a % 400 == 0:
     print("El año es bisiesto.")
 elif a % 4 == 0 and a % 100 != 0:
-    print("El año es bisiesto.")
+    print("E we si we, ya cheque bein we, el año si es bisiesto año es bisiesto.")
 else:
-    print("El año no es bisiesto.")
+    print("Oye, no we, ya cheque bien we y no we el año no es bisiesto.")
+
