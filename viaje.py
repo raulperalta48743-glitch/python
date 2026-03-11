570cdda0c810c4eb351b5aa3e0730cf35de4525b
raulperalta48743-glitch <raulperalta48743@gmail.com>
2026-03-11 14:07:54 -0600

    Implement travel destination selection prompts
    Add initial travel destination prompts and options.

---
+++ viaje.py
@@ -0,0 +1,9 @@
+print  ("que onda we, somos viajes el cabron del norte. A donde vas?")
+print ("Viajes el cabron del norte te lleva a:")
+print ("1.-Monterrey-----------------------$950")
+print ("2.-Guadalajara-----------------------$850")
+print ("3.-Cancun.-----------------------$1200")
+dondevaswe = in(input("/n ingrese el destino al que quiere ir (numero0:)"))
+
+match destino:
+    case1:
